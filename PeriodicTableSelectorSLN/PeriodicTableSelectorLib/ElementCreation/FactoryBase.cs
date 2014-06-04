9b0c25e8373ebf7559940f26a92149e177f90de7
francisco-chavez <francisco.chavez.509@outlook.com>
2014-06-04 14:01:34 -0700

    Chemical Factory
    Started working figuring out which functions the factory base needs to
    provide to the periodic table. I've also started implementing those
    functions in the standard table factory class.

--- PeriodicTableSelectorSLN/PeriodicTableSelectorLib/ElementCreation/FactoryBase.cs
+++ PeriodicTableSelectorSLN/PeriodicTableSelectorLib/ElementCreation/FactoryBase.cs
@@ -8,5 +8,14 @@ namespace Unv.PeriodicTableSelectorLib.ElementCreation
 {
 	public abstract class FactoryBase
 	{
+		public abstract ChemicalElement[]	Elements		{ get; }
+		public abstract string[]			ElementNames	{ get; }
+		public abstract string[]			ElementSymbols	{ get; }
+
+
+		public abstract bool			HasElement(int atomicNumber);
+		public abstract bool			HasElement(string nameOrSymbol);
+		public abstract ChemicalElement Element(int atomicNumber);
+		public abstract ChemicalElement Element(string nameOrSymbol);
 	}
 }
--- PeriodicTableSelectorSLN/PeriodicTableSelectorLib/ElementCreation/StandardTableFactory.cs
+++ PeriodicTableSelectorSLN/PeriodicTableSelectorLib/ElementCreation/StandardTableFactory.cs
@@ -9,5 +9,111 @@ namespace Unv.PeriodicTableSelectorLib.ElementCreation
 	public class StandardTableFactory
 		: FactoryBase
 	{
+		#region Attributes
+		private Dictionary<int, ChemicalElement>	m_atomicNumberIndex;
+		private Dictionary<string, ChemicalElement> m_nameIndex;
+
+		private				ChemicalElement[]		m_elements;
+		private readonly	string[]				m_elementNames;
+		private readonly	string[]				m_elementSymbols;
+		#endregion
+
+
+		#region Properties
+		public override ChemicalElement[] Elements
+		{
+			get
+			{
+				ChemicalElement[] copy = new ChemicalElement[m_elements.Length];
+				m_elements.CopyTo(copy, 0);
+				return copy;
+			}
+		}
+
+		public override string[] ElementNames
+		{
+			get { return m_elementNames; }
+		}
+
+		public override string[] ElementSymbols
+		{
+			get { return m_elementSymbols; }
+		}
+		#endregion
+
+
+		#region Constructors
+		public StandardTableFactory()
+		{
+			m_atomicNumberIndex = new Dictionary<int, ChemicalElement>(118);
+			m_nameIndex			= new Dictionary<string, ChemicalElement>(236);
+
+			var elements = CreateElements();
+
+			m_elementNames		= elements.Select(chem => { return chem.ChemicalName; }).ToArray();
+			m_elementSymbols	= elements.Select(chem => { return chem.Symbol; }).ToArray();
+
+			foreach (var element in elements)
+			{
+				m_atomicNumberIndex.Add(element.AtomicNumber, element);
+				m_nameIndex.Add(element.ChemicalName.ToLower(), element);
+				m_nameIndex.Add(element.Symbol.ToLower(), element);
+			}
+		}
+		#endregion
+
+
+		#region Methods
+		public override bool HasElement(int atomicNumber)
+		{
+			return m_atomicNumberIndex.ContainsKey(atomicNumber);
+		}
+
+		public override bool HasElement(string nameOrSymbol)
+		{
+			return m_nameIndex.ContainsKey(nameOrSymbol.ToLower());
+		}
+
+		public override ChemicalElement Element(int atomicNumber)
+		{
+			return m_atomicNumberIndex[atomicNumber];
+		}
+
+		public override ChemicalElement Element(string nameOrSymbol)
+		{
+			return m_nameIndex[nameOrSymbol.ToLower()];
+		}
+
+		private List<ChemicalElement> CreateElements()
+		{
+			List<ChemicalElement> elements = new List<ChemicalElement>(118);
+
+			///
+			/// Elements 1 - 5
+			/// 
+			CreateNewElement(1, "H",  "Hydrogen",	 1.008,		elements);
+			CreateNewElement(2, "He", "Helium",		 4.002602,	elements);
+			CreateNewElement(3, "Li", "Lithium",	 4.94,		elements);
+			CreateNewElement(4, "Be", "Beryllium",   9.012182,	elements);
+			CreateNewElement(5, "B",  "Boron",		10.81,		elements);
+
+
+			return elements;
+		}
+
+		private void CreateNewElement(int atomicNumer, string symbol, string name, double meanAtomicMass, List<ChemicalElement> elements)
+		{
+			var element = 
+				new ChemicalElement() 
+				{ 
+					AtomicNumber	= atomicNumer, 
+					Symbol			= symbol, 
+					ChemicalName	= name, 
+					AtomicMass		= meanAtomicMass 
+				};
+
+			elements.Add(element);
+		}
+		#endregion
 	}
 }
