c8e2b9a10ab67536eb8c328b927cfd73be3510d3
francisco-chavez <francisco.chavez.509@outlook.com>
2014-06-17 15:17:34 -0700

    Group Manipulator
    I've created the public methods that the Group Manipulator will need for
    what I want it to do.

--- PeriodicTableSelectorSLN/TestDemo01/Examples/Tools/GroupManipulator.xaml.cs
+++ PeriodicTableSelectorSLN/TestDemo01/Examples/Tools/GroupManipulator.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Unv.PeriodicTableSelectorLib;
+
 
 namespace TestDemo01.Examples.Tools
 {
@@ -22,10 +25,80 @@ namespace TestDemo01.Examples.Tools
 	public partial class GroupManipulator 
 		: UserControl
 	{
+		#region Events
+		public event PropertyChangedEventHandler PropertyChanged;
+		#endregion
+
+
+		#region Properties
+		public string GroupName
+		{
+			get { return mn_groupName; }
+			set
+			{
+				if (mn_groupName != value)
+				{
+					mn_groupName = value;
+					OnPropertyChanged("GroupName");
+				}
+			}
+		}
+		private string mn_groupName;
+
+		public ChemicalGroup Group
+		{
+			get { return mn_group; }
+			set
+			{
+				if (mn_group != value)
+				{
+					mn_group = value;
+					OnPropertyChanged("Group");
+
+					if (value != null)
+					{
+						GroupName = value.GroupName;
+					}
+				}
+			}
+		}
+		private ChemicalGroup mn_group;
+		#endregion
+
+
+		#region Constructors
 		public GroupManipulator()
 		{
 			InitializeComponent();
 			DataContext = this;
 		}
+		#endregion
+
+
+		#region Event Handlers
+		private void ChangeBackground_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void ChangeForeground_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void ChangeGlowBrush_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+		#endregion
+
+
+		#region Methods
+		private void OnPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+		#endregion
 	}
 }
