bb6e73e072ac40325d940b20d5a02f316881b05f
francisco-chavez <francisco.chavez.509@outlook.com>
2014-06-17 15:03:22 -0700

    Adding Class that Plays with Chemical Groups
    I'm adding a class that will reference and give out commands to Chemical
    Groups. It will also display the name of the Chemical Group it is
    working with.

---
+++ PeriodicTableSelectorSLN/TestDemo01/Examples/Tools/GroupManipulator.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace TestDemo01.Examples.Tools
+{
+	/// <summary>
+	/// Interaction logic for GroupManipulator.xaml
+	/// </summary>
+	public partial class GroupManipulator 
+		: UserControl
+	{
+		public GroupManipulator()
+		{
+			InitializeComponent();
+			DataContext = this;
+		}
+	}
+}
