4009594cb521093eb3ac3c7c0fa906040582263b
francisco-chavez <francisco.chavez.509@outlook.com>
2014-07-16 16:33:42 -0700

    Started work on new Example for Demo app
    While showing off the demo up, I came across a color combination that
    looked really nice. By making a few additions to the Halloween example,
    make it so that the Halloween example is able to recreate that color
    combination with just a few settings.

--- PeriodicTableSelectorSLN/TestDemo01/Examples/Halloween.xaml.cs
+++ PeriodicTableSelectorSLN/TestDemo01/Examples/Halloween.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Unv.PeriodicTableSelectorLib;
 
 
 namespace TestDemo01.Examples
@@ -18,8 +19,43 @@ namespace TestDemo01.Examples
 	/// <summary>
 	/// Interaction logic for Halloween.xaml
 	/// </summary>
-	public partial class Halloween : UserControl
+	public partial class Halloween 
+		: UserControl
 	{
+		#region Attributes
+		public readonly static DependencyProperty GlowBrushProperty;
+		public readonly static DependencyProperty TextBrushProperty;
+		public readonly static DependencyProperty BackgroundBrushProperty;
+		#endregion
+
+
+		#region Properties
+		public Brush GlowBrush
+		{
+			get { return (Brush) GetValue(GlowBrushProperty); }
+			set { SetValue(GlowBrushProperty, value); }
+		}
+
+		public Brush TextBrush
+		{
+			get { return (Brush) GetValue(TextBrushProperty); }
+			set { SetValue(TextBrushProperty, value); }
+		}
+
+		public Brush BackgroundBrush
+		{
+			get { return (Brush) GetValue(BackgroundBrushProperty); }
+			set { SetValue(BackgroundBrushProperty, value); }
+		}
+		#endregion
+
+
+		#region Constructors
+		static Halloween()
+		{
+
+		}
+
 		public Halloween()
 		{
 			InitializeComponent();
@@ -31,5 +67,20 @@ namespace TestDemo01.Examples
 
 			group.SelectChemicals();
 		}
+		#endregion
+
+
+		#region Dependency Property Event Handlers
+
+		#endregion
+
+
+		#region Methods
+		private ChemicalGroup GetAllChemicalsGroup()
+		{
+			var group = this.Selector.ChemicalElementFactory.ChemicalGroups.First(g => { return g.GroupName == "All Chemical Elements"; });
+			return group;
+		}
+		#endregion
 	}
 }
